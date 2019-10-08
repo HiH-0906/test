36c8ec040f94ac6e0edd3555c08d973ff4887f09
HiH-0906 <homuihomu0906@gmail.com>
2019-10-08 19:52:40 +0900

    ｲｼﾞﾘ用

---
+++ Project5 2/Player.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Obj.h"
+class Player :
+	public Obj
+{
+public:
+	Player();
+	~Player();
+private:
+	void Init(void);
+};
+
---
+++ Project5 2/Scene/GameScene.cpp
@@ -0,0 +1,47 @@
+#include <DxLib.h>
+#include <_DebugConOut.h>
+#include "GameScene.h"
+#include <ImageMng.h>
+#include <Player.h>
+#include <Enemy.h>
+
+
+GameScene::GameScene()
+{
+	lpImageMng.GetID("·¬×", "image/char.png", { 30,32 }, { 10, 10 });
+	lpImageMng.GetID("’e", "image/shot.png", {8, 3}, { 1, 2});
+	lpImageMng.GetID("“G”š”­", "image/en_blast.png", { 64,64 }, { 5, 1 });
+	lpImageMng.GetID("PL”š”­", "image/pl_blast.png", { 64,64 }, { 4, 1 });
+
+	TREACE("¹Þ°Ñ¼°Ý‚Ì¶¬");
+	obj.resize(2);
+
+	obj[0] = new Player();
+	obj[0]->animKey("NORMAL");
+
+	obj[0]->pos(60, 60);
+
+	obj[1] = new Enemy();
+	obj[1]->animKey("NORMAL");
+
+	obj[1]->pos(160, 60);
+
+
+	/*obj[0] = new Obj("image/char.png", { 0,0 }, 10, 10, 30, 32);
+	obj[1] = new Obj("image/char.png", { 500,100 }, 10, 10, 30, 32);*/
+}
+
+
+GameScene::~GameScene()
+{
+}
+
+unipueBase GameScene::Update(unipueBase own)
+{
+	// æ“ª‚ðŽæ‚èo‚µAÅŒã‚Ì—v‘f‚Ü‚Å‰ñ‚µ‚Ä‚­‚ê‚é
+	for(auto data :obj)
+	{
+		data->Draw();
+	}
+	return std::move(own);
+}
